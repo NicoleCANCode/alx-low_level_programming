45183c2c1d3cd9543f0a16e2377e022297d0d5f1
NicoleCANCode <marneweckn@gmail.com>
2023-02-21 11:43:48 -0800

    ai betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_times_table : print the n times table, starting with 0
+ * main print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
 void print_times_table(int n)
