b2a1614bfa837a774688d5310e86e19cefe4bc06
NicoleCANCode <marneweckn@gmail.com>
2023-03-14 14:17:46 -0700

    fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,11 +8,12 @@
 int count_word(char *s)
 {
 	int flag, c, w;
+	
 	flag = 0;
 	w = 0;
 	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (s[c] == ' ')
+	if (s[c] == ' ')
 	flag = 0;
 else
 	if (flag == 0)
@@ -33,6 +34,7 @@ char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
+
 	while (*(str + len))
 		len++;
 	words = count_word(str);
